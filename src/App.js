0d4ee6661f42764d91d6360e9f91305047e1f354
enigm413 <rohithkkannikath@gmail.com>
2023-12-21 08:18:36 +0530

    update the component using reusable button and step message

--- src/App.js
+++ src/App.js
@@ -51,21 +51,53 @@ export default function App() {
           </div>
 
           {/* Display the current message based on stepNumber */}
-          <div className="message">{`${stepNumber}:${
-            messages[stepNumber - 1]
-          }`}</div>
+          <StepMessage stepNumber={stepNumber}>
+            {messages[stepNumber - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() =>
+                  alert(`Learn how to ${messages[stepNumber - 1]}`)
+                }
+              >
+                Learn How{" "}
+              </Button>
+            </div>
+          </StepMessage>
 
           {/* Buttons to navigate between steps */}
           <div className="buttons">
-            <button className="btn" onClick={handlePrevious}>
-              Previous
-            </button>
-            <button className="btn" onClick={handleNext}>
-              Next
-            </button>
+            <Button onClick={handlePrevious} bgColor="#7950f2" textColor="#fff">
+              <span>👈 Previous</span>
+            </Button>
+
+            <Button onClick={handleNext} bgColor="#7950f2" textColor="#fff">
+              <span>Next 👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ onClick, textColor, bgColor, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function StepMessage({ stepNumber, children }) {
+  return (
+    <p className="message">
+      <h3>Step {stepNumber}</h3>
+      {children}
+    </p>
   );
 }
